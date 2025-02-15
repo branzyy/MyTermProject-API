0ecce7aef9237d313638ce007433e5d14f8e9b9f
Brandon <146174490+branzyy@users.noreply.github.com>
2025-02-15 13:58:43 +0300

    changes

--- purchase.php
+++ purchase.php
@@ -64,7 +64,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             'purchaseID' => $purchaseID,
             'vehiclename' => $vehiclename,
             'purchasedate' => $purchasedate,
-            'email' => $userEmail,
+            'email' => $userEmail
         ];
 
     
